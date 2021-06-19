13512f21d9092eec59a46dd709dfd7219b09c13f
jeflarremore <45082609+jeflarremore@users.noreply.github.com>
2021-06-19 18:19:13 -0500

    Add project files.

---
+++ SQLSamples/FindTop-N-Salary.sql
@@ -0,0 +1,25 @@
+﻿
+-- Assumes the following employees table structure
+--CREATE TABLE employees (
+--	employee_id INT IDENTITY(1,1) PRIMARY KEY,
+--	first_name VARCHAR (20) DEFAULT NULL,
+--	last_name VARCHAR (25) NOT NULL,
+--	email VARCHAR (100) NOT NULL,
+--	phone_number VARCHAR (20) DEFAULT NULL,
+--	hire_date DATE NOT NULL,
+--	job_id INT NOT NULL,
+--	salary DECIMAL (8, 2) NOT NULL,
+--	manager_id INT DEFAULT NULL,
+--	department_id INT DEFAULT NULL,
+--	FOREIGN KEY (job_id) REFERENCES jobs (job_id) ON DELETE CASCADE ON UPDATE CASCADE,
+--	FOREIGN KEY (department_id) REFERENCES departments (department_id) ON DELETE CASCADE ON UPDATE CASCADE,
+--	FOREIGN KEY (manager_id) REFERENCES employees (employee_id)
+--);
+
+SELECT Top(1) a.salary 
+From (
+	Select Top(13) salary 
+	From employees
+	Order By salary DESC
+) AS a
+Order by a.salary ASC
